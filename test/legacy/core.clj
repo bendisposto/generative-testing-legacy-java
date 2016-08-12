e65c753b4fb6d590bb1efbffcf6e0e3b404cd4cf
Jens Bendisposto <jens@bendisposto.de>
2016-08-12 12:11:37 +0200

    - replay shrinked example - show failing sequence - only consider the results until the first Exception occurs

--- test/legacy/core.clj
+++ test/legacy/core.clj
@@ -3,7 +3,7 @@
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.reflect :as reflect]
-            [clojure.test.check.clojure-test :refer [defspec]])
+            [clojure.test :refer [deftest is]])
   (:import [java.io ByteArrayOutputStream PrintStream]))
 
 ;; Trivia Game Interpreter
@@ -51,8 +51,22 @@
   (conj trace
         (capture-output (.isPlayable game))))
 
+
+(defn abstract-output [{:keys [output result] :as k}])
+
+
+(defn- run-full [game sequence]
+   (reduce (partial run-cmd game) [] sequence))
+
+(defn- exception? [{r :result}]
+  (and (map? r)
+       (:exception r)))
+
 (defn run [game sequence]
-  (reduce (partial run-cmd game) [] sequence))
+  (let [r (run-full game sequence)
+        [pre [e & _]] (partition-by exception? r)
+        p (concat pre [e])]
+        p))
 
 
 ;; Generation
@@ -70,19 +84,31 @@
                        (gen/return [:howManyPlayers])]))
 
 (def program
-   (gen/vector step 50 100))
+   (gen/vector step))
 
 ;; Properties
 
+(defn run-refactored [p]
+  (run (new com.adaptionsoft.games.trivia.Game) p))
+
+(defn run-original [p]
+  (run (new com.adaptionsoft.games.uglytrivia.Game) p))
+
+
+
 (def equivalence
   (prop/for-all [p program]
-                (let [r (run (new com.adaptionsoft.games.trivia.Game) p)
-                      r' (run (new com.adaptionsoft.games.uglytrivia.Game) p)]
+                (let [r (run-refactored p)
+                      r' (run-original p)]
                   (= r r'))))
 
-(defspec refactoring-should-preserve-behavior
-  100
-  equivalence)
+
+(deftest refactoring-should-preserve-behavior
+  (let [{:keys [result seed] :as r} (tc/quick-check 500 equivalence)]
+    (when (not= result :ok)
+      (let [[p & _] (get-in r [:shrunk :smallest])]
+        (is (= (run-original p) (run-refactored p)) (str "Sequence: " p))))))
+
 
 ;;(println (last (gen/sample program 1500)))
 
